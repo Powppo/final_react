4cf34c7fba92a5881c381f9462ed53186f9c7a96
Powppo <mafrizal575@gmail.com>
2023-05-15 20:07:56 +0700

    Phase 1 Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editx <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login/>
+        </Route>
+        <Route path="/Register">
+          <Register/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+      
   );
 }
 
