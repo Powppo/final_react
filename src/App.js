6835371c850580164d1bef444213b3a7aa09d183
Powppo <mafrizal575@gmail.com>
2023-05-17 11:42:52 +0700

    Databased Register Data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/">
-          <Login/>
-        </Route>
-        <Route path="/Register">
-          <Register/>
-        </Route>
+      <Route exact path='/' element={<Login/>} />
+        <Route path='/Register' element={<Register/>} />
+        <Route path='/Dashboard' element={[<Navbar/>, <Dashboard/>]}/>
       </Routes>
     </BrowserRouter>
       
