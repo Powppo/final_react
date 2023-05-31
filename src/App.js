9448e9dd44c53ee6c45ca4c8b7ac4b704f5fa581
lelyta30 <salsabilalelyta@gmail.com>
2023-05-31 09:02:18 +0700

    user

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
-
+import User from "./components/User";
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <Route exact path='/' element={<Login/>} />
         <Route path='/Register' element={<Register/>} />
         <Route path='/Dashboard' element={[<Navbar/>, <Dashboard/>]}/>
+        <Route path='/user' element={<User/>}/>
+
       </Routes>
     </BrowserRouter>
       
--- src/components/user/user.js
+++ src/components/user/user.js
@@ -4,6 +4,7 @@ const User = () => {
     const [nilai, setNilai] = useState('');
     const [sertif, setSertif] = useState('');
     const [sekolah, setSekolah] = useState('');
+
     const User = async (e) => {
         e.preventDefault();
         await axios.post('http://localhost:5000/users', {
@@ -16,42 +17,43 @@ const User = () => {
             console.log(err)
         })
     }
-    
+
     return (
         <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-        <div className="hero-body">
-        <div className="container">
-        <div className="column is-centered">
-        <div className="column is-4-desktop">
-            <form onSubmit={User} className="box">
-            <p className='has-text-centered'>{msg}</p>
-            <div className="field mt-5">
-                        <label className="label">Nilai</label>
-                        <div className="controls">
-                            <input type="text" className="input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <div className="hero-body">
+                <div className="container">
+                    <div className="column is-centered">
+                        <div className="column is-4-desktop">
+                            <form onSubmit={User} className="box">
+                                <p className='has-text-centered'>{msg}</p>
+                                <div className="field mt-5">
+                                    <label className="label">Nilai</label>
+                                    <div className="controls">
+                                        <input type="text" className="input" placeholder="Nilai" value={nilai} onChange={(e) => setNilai(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Sertif</label>
+                                    <div className="controls">
+                                        <input type="text" className="input" placeholder="Sertifikat" value={sertif} onChange={(e) => setSertif(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Sekolah</label>
+                                    <div className="controls">
+                                        <input type="text" className="input" placeholder="Sekolah" value={sekolah} onChange={(e) => setSekolah(e.target.value)} />
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <button className="button is-success is-fullwidth">User</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                    <div className="field mt-5">
-                        <label className="label">Sertif</label>
-                        <div className="controls">
-                            <input type="text" className="input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="field mt-5">
-                        <label className="label">Sekolah</label>
-                        <div className="controls">
-                            <input type="text" className="input" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="field mt-5">
-                        <button className="button is-success is-fullwidth">User</button>
-                    </div>
-                </form>
+                </div>
             </div>
-            </div>
-        </div>
-        </div>
-    </section>
+        </section>
     )
+}
 
 export default User
